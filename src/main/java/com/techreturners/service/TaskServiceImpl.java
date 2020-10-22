505216ac609e5aeb101b146e12d2635d1d0c7110
James Heggs <james.heggs@techreturners.com>
2020-10-22 17:45:31 +0100

    Introducing mocking and a successful test

--- src/main/java/com/techreturners/service/TaskServiceImpl.java
+++ src/main/java/com/techreturners/service/TaskServiceImpl.java
@@ -4,12 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techreturners.model.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 public class TaskServiceImpl implements TasksService{
---
+++ src/test/java/com/techreturners/service/TaskServiceTest.java
@@ -0,0 +1,46 @@
+package com.techreturners.service;
+
+import com.techreturners.model.Task;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceTest {
+
+    @Mock
+    private Connection mockConnection;
+
+    @Mock
+    PreparedStatement mockPreparedStatement;
+
+    @Mock
+    ResultSet mockResultSet;
+
+    @InjectMocks
+    private TaskServiceImpl taskService;
+
+    @Test
+    void testFetchTasksSuccessfullyReturnsList() throws Exception {
+        Mockito.mockStatic(DriverManager.class);
+        Mockito.when(DriverManager.getConnection(Mockito.anyString())).thenReturn(mockConnection);
+        Mockito.when(mockConnection.prepareStatement(Mockito.anyString())).thenReturn(mockPreparedStatement);
+        Mockito.when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
+        Mockito.when(mockResultSet.next()).thenReturn(true).thenReturn(false);
+        Mockito.when(mockResultSet.getString("taskId")).thenReturn("ABC1234");
+
+        List<Task> tasks = taskService.getTasks("user1234");
+        assertEquals(1, tasks.size());
+    }
+}
